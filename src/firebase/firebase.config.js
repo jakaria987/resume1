5e625ddb647363faa0e044192dffebdb5a75d111
Md Jakaria <jakhossain999@gmail.com>
2023-05-02 00:34:39 +0600

    route setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC97vYq0qNpX1a0G_8fQm9zLhAz1e3QFRI",
+  authDomain: "assignment10-71e51.firebaseapp.com",
+  projectId: "assignment10-71e51",
+  storageBucket: "assignment10-71e51.appspot.com",
+  messagingSenderId: "1015167080062",
+  appId: "1:1015167080062:web:66adae64595a33b504aa2f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Shared/Header/Header';
+import Footer from '../components/Shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
